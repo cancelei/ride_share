e94131a0f8c126b931ddfda9cee334a6100835a2
cancelei <glauber.bannwart@gmail.com>
2025-01-19 09:49:25 -0600

    feat/configure user

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum :role, { admin: 0, driver: 1, passenger: 2 }
+end
