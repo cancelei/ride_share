13da58ec1f9b2af9486eab42b47e00d9980f3a87
cancelei <glauber.bannwart@gmail.com>
2025-02-05 10:54:27 -0600

    feat/add admin dashboard upgrades and users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
+
+  resources :users
+
   root "dashboard#show"
 
   post "driver/update_location", to: "driver_profiles#update_location"
