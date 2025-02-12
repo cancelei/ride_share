d3018e4975df9cac2a951a2885044f0a23431cac
cancelei <glauber.bannwart@gmail.com>
2025-02-12 00:05:47 -0600

    feat/removes cash payment option and validates at least 1 payment method for driver_profiles.

--- app/models/driver_profile.rb
+++ app/models/driver_profile.rb
@@ -7,12 +7,18 @@ class DriverProfile < ApplicationRecord
   has_many :rides, -> { with_discarded }, foreign_key: "driver_id", dependent: :destroy
 
   validates :license, :license_issuer, presence: true
-
+  validate :at_least_one_payment_address
   validate :only_one_active_driver_profile_per_user, on: :create
 
   def only_one_active_driver_profile_per_user
     if DriverProfile.where(user: user).count > 0
       errors.add(:base, "Only one active driver profile per user is allowed")
+    end
+  end
+
+  def at_least_one_payment_address
+    unless bitcoin_address.present? || ethereum_address.present? || icc_address.present?
+      errors.add(:base, "At least one payment address (Bitcoin, Ethereum, or ICC) must be provided.")
     end
   end
 
