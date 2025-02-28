e84ca3d0b3e77688c55ca63d852838f4d0f429de
cancelei <glauber.bannwart@gmail.com>
2025-02-28 12:59:05 -0600

    feat/update pricing

--- app/models/concerns/price_calculator.rb
+++ app/models/concerns/price_calculator.rb
@@ -3,7 +3,7 @@ module PriceCalculator
 
   def calculate_estimated_price
     base_fare = 5
-    per_km_rate = 1.92
+    per_km_rate = 1.7
 
     (base_fare + (distance_km.to_f * per_km_rate)).round(2)
   end
