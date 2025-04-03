535913a33158676549e8f0add06eb83eeb5b8a16
cancelei <glauber.bannwart@gmail.com>
2025-04-03 17:42:25 -0300

    Revert "feat/adds posthog via yarn with gitignore update"
    This reverts commit bc596ba36ff7d89ceb09c3a09018f1794847d611.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// Initialize PostHog
-import posthog from 'posthog-js'
-
-posthog.init('phc_DPxPX8BS8ejaf1Z6pILVCYCoiWre27rth7QmYmFmH2D', {
-    api_host: 'https://us.i.posthog.com',
-    person_profiles: 'identified_only'
-})
