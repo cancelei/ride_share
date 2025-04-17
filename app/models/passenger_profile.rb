80eea1be569f3cf425202b158d19520a003dd436
Usama Tahreem <usama.tahreem@orbixtechnologies.com>
2025-04-17 22:23:28 +0500

    feat/validate now validating the whatsapp number

--- app/models/passenger_profile.rb
+++ app/models/passenger_profile.rb
@@ -9,6 +9,8 @@ class PassengerProfile < ApplicationRecord
 
   validate :only_one_active_passenger_profile_per_user, on: :create
 
+  validates :whatsapp_number, phone: true
+
   def only_one_active_passenger_profile_per_user
     if PassengerProfile.where(user: user).count > 0
       errors.add(:base, "Only one active passenger profile per user is allowed")
