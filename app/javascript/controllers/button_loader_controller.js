d0c92aa47698ffd0e9e49709396618578dfbb460
cancelei <glauber.bannwart@gmail.com>
2025-05-03 15:40:20 -0300

    feat/bug_fix loader

--- app/javascript/controllers/button_loader_controller.js
+++ app/javascript/controllers/button_loader_controller.js
@@ -5,8 +5,16 @@ export default class extends Controller {
   
   connect() {
     console.log("Button loader controller connected", this.element)
+    
+    // Store original disable-with value if exists
+    if (this.element.hasAttribute("data-disable-with")) {
+      this.originalDisableWith = this.element.getAttribute("data-disable-with")
+      // Remove the default Rails disable-with behavior to avoid conflicts
+      this.element.removeAttribute("data-disable-with")
+    }
+    
     // Use the turbo events to show/hide loader
-    this.element.addEventListener("click", this.showLoader.bind(this))
+    this.element.addEventListener("click", this.handleClick.bind(this))
     
     // Event listeners for Turbo form submissions
     document.addEventListener("turbo:submit-start", this.handleTurboSubmitStart.bind(this))
@@ -26,8 +34,14 @@ export default class extends Controller {
   
   disconnect() {
     console.log("Button loader controller disconnected", this.element)
+    
+    // Restore original disable-with value if it existed
+    if (this.originalDisableWith) {
+      this.element.setAttribute("data-disable-with", this.originalDisableWith)
+    }
+    
     // Clean up event listeners
-    this.element.removeEventListener("click", this.showLoader.bind(this))
+    this.element.removeEventListener("click", this.handleClick.bind(this))
     document.removeEventListener("turbo:submit-start", this.handleTurboSubmitStart.bind(this))
     document.removeEventListener("turbo:submit-end", this.handleTurboSubmitEnd.bind(this))
     document.removeEventListener("turbo:before-fetch-request", this.handleTurboFetchRequest.bind(this))
@@ -40,8 +54,46 @@ export default class extends Controller {
     window.removeEventListener("error", this.handleError.bind(this))
   }
   
-  showLoader(event) {
-    console.log("showLoader called", event)
+  // Handle initial button click
+  handleClick(event) {
+    console.log("Button clicked", event)
+    
+    // For non-Turbo forms, directly handle the click event
+    if (this.isFormButton() && this.element.form && this.element.form.getAttribute("data-turbo") === "false") {
+      console.log("Non-Turbo form detected")
+      this.showLoader()
+      
+      // Add a page unload handler to ensure we reset button state if navigating away
+      window.addEventListener("beforeunload", this.handleBeforeUnload.bind(this), { once: true })
+      
+      // Don't disable the button for non-Turbo forms, let the browser handle it
+      this.element.disabled = false
+      
+      // Ensure button is re-enabled if form submission wasn't received
+      this.loadingTimeout = setTimeout(() => {
+        console.log("Failsafe timeout triggered")
+        this.hideLoader()
+      }, 10000)
+      
+      return true // Allow the form to submit normally
+    }
+    
+    // For all other cases, use the standard behavior
+    // But don't disable buttons on regular link/button clicks unless form-related
+    if (this.isFormButton() || this.element.tagName === "A") {
+      this.showLoader()
+    }
+  }
+  
+  // Handle page unload for form submission
+  handleBeforeUnload(event) {
+    console.log("Page unloading")
+    window.removeEventListener("beforeunload", this.handleBeforeUnload)
+    clearTimeout(this.loadingTimeout)
+  }
+  
+  showLoader() {
+    console.log("showLoader called for", this.element)
     // Only add loader if this is a form submission or link button
     if (this.isFormButton() || this.element.tagName === "A" || this.element.tagName === "BUTTON") {
       console.log("Adding loading class to", this.element)
@@ -52,17 +104,31 @@ export default class extends Controller {
         this.spinnerTarget.classList.remove("hidden")
       }
       
-      // Disable the button while loading, but store its previous state
-      this.wasDisabled = this.element.disabled
-      this.element.disabled = true
-      console.log("Button disabled", this.element)
+      // For AJAX and Turbo forms, disable the button
+      // For regular form submissions, don't disable to allow the form to submit
+      if (!this.isRegularFormSubmit()) {
+        // Disable the button while loading, but store its previous state
+        this.wasDisabled = this.element.disabled
+        this.element.disabled = true
+        console.log("Button disabled", this.element)
+      }
       
       // Make sure we re-enable after a timeout as a failsafe
+      if (this.loadingTimeout) {
+        clearTimeout(this.loadingTimeout)
+      }
+      
       this.loadingTimeout = setTimeout(() => {
         console.log("Failsafe timeout triggered")
         this.hideLoader()
       }, 10000) // 10 second failsafe timeout
     }
+  }
+  
+  isRegularFormSubmit() {
+    return this.isFormButton() && 
+           this.element.form && 
+           this.element.form.getAttribute("data-turbo") === "false"
   }
   
   hideLoader() {
@@ -99,11 +165,8 @@ export default class extends Controller {
   // Handle traditional form submission
   handleFormSubmit(event) {
     console.log("Form submit event", event)
-    // Only handle if not processed by Turbo
-    if (!window.Turbo || !window.Turbo.navigator) {
-      console.log("Non-Turbo form submission")
-      this.showLoader()
-    }
+    // Always show loader on form submit
+    this.showLoader()
   }
   
   // Handle turbo:submit-start event
