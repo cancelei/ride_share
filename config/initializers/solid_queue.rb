19b07d0d016557b5e818384b2ccf841bb64b76c3
cancelei <glauber.bannwart@gmail.com>
2025-07-17 10:39:14 -0300

    feat/update entry

--- config/initializers/solid_queue.rb
+++
@@ -1,7 +0,0 @@
-# Configure database connection
-
-Rails.application.config.solid_queue.connects_to = {
-  database: { writing: :queue }
-}
-# Configure mailers queue
-Rails.application.config.action_mailer.deliver_later_queue_name = :mailers
